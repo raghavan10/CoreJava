cfca3a5f662f0860ad2b37882d3a5fc494211402
USER <USER@DESKTOP-PRU93D6>
2024-06-25 18:50:48 +0530

    first commit

---
+++ JavaCore/src/com/tnsif/encapsulationprogram/Encapsulation.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulationprogram;
+
+public class Encapsulation {
+	int serialNum=10;//Data variables
+	String name;
+	int age;
+	
+	void show()//Member function 
+	{
+		System.out.println("serialNumber"+serialNum+"\t Name"+name+"\t Age:"+age);
+	}
+}
